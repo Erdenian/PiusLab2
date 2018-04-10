c2ecdb6aae4f6b4119b5edb1c3235b1537f4cdbd
Ilya Solovyev <mail@erdenian.ru>
2018-04-10 17:48:56 +0300

    Добавлен MainApplication

---
+++ src/main/java/ru/mp45/piuslab2/MainApplication.java
@@ -0,0 +1,24 @@
+package ru.mp45.piuslab2;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class MainApplication extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("/layouts/main.fxml"));
+        Scene scene = new Scene(root, 800.0, 480.0);
+
+        primaryStage.setTitle("Кирилл лох");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+}
---
+++ src/main/java/ru/mp45/piuslab2/MainController.java
@@ -0,0 +1,11 @@
+package ru.mp45.piuslab2;
+
+import javafx.fxml.FXML;
+
+public class MainController {
+
+    @FXML
+    private void initialize() {
+        System.out.println("Started");
+    }
+}
