e31099879c696731d9160c77a9bbdc5ec5738e36
lainys <is-kirillka@yandex.ru>
2018-04-18 14:13:52 +0300

    сделал 3 панель + контролер

--- src/main/java/ru/mp45/piuslab2/controllers/ThirdTab.java
+++ src/main/java/ru/mp45/piuslab2/controllers/ThirdTab.java
@@ -15,7 +15,7 @@ public class ThirdTab {
     private LineChart chart_first_porog;
 
     @FXML
-    private BarChart bar_fisrt;
+    private BarChart hist_first;
 
     @FXML
     private LineChart chart_second;
@@ -24,7 +24,7 @@ public class ThirdTab {
     private LineChart chart_second_porog;
 
     @FXML
-    private BarChart bar_second;
+    private BarChart hist_second;
 
     @FXML
     private void initialize() {
@@ -32,19 +32,25 @@ public class ThirdTab {
         int N = 1000;
 
         double[] points = Generator.getRandomPoints(N);
-
         double[] white_noisy = Generator.whiteNoisy(points);
+        double[] rxx = Generator.rxx(white_noisy);
+        double[][] porog = Generator.getPorog(1000,white_noisy);
+
+        for(int i =0;i<rxx.length;i++){
+            System.out.println(rxx[i]);
+        }
+
+        DrawChart.setValueToChart(chart_first,white_noisy);
+        DrawChart.setValueToChart2(chart_first_porog,porog);
+        DrawChart.setValueToHist(hist_first,rxx);
 
         double[] color_noisy = Generator.colorNoisy(white_noisy);
-
-        DrawChart.setValueToChart(chart_first,points);
-
-        double[][] porog = Generator.getPorog(points);
-
-
-        DrawChart.setValueToChart2(chart_first_porog,porog);
+        double[] rxx_color = Generator.rxx(color_noisy);
+        double[][] porog_color = Generator.getPorog(1000,color_noisy);
 
         DrawChart.setValueToChart(chart_second,color_noisy);
+        DrawChart.setValueToChart2(chart_second_porog,porog_color);
+        DrawChart.setValueToHist(hist_second,rxx_color);
 
     }
 
