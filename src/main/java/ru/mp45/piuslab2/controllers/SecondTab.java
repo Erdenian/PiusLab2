9e90be4e6e0c685b25b9f16a0f29cca7cc2344c3
lainys <is-kirillka@yandex.ru>
2018-04-23 15:47:11 +0300

    добавил коментарии

--- src/main/java/ru/mp45/piuslab2/controllers/SecondTab.java
+++ src/main/java/ru/mp45/piuslab2/controllers/SecondTab.java
@@ -30,25 +30,31 @@ public class SecondTab {
     @FXML
     private BarChart hist_third;
 
-
+    // общее количество точек
     private int N = 1000;
 
 
     @FXML
     private void initialize() {
 
+        // получаем реализацию случайных чисел
         double[] points = getRandomPoints(N);
 
+        // рисуем график и гистограмму для случайных чисел
         setValueToChart(chart_first, points);
         setValueToHist(hist_first, points);
 
+        // поулчаем реализацию белого шума
         double[] white_noisy = whiteNoisy(points);
 
+        // рисуем график и гистограмму дяля белого шума
         setValueToChart(chart_second, white_noisy);
         setValueToHist(hist_second, white_noisy);
 
+        // получаем реализацию окрашенного шума
         double[] color_noisy = colorNoisy(white_noisy);
 
+        // рисуем график и гистограмму окрашенного шума
         setValueToChart(chart_third, color_noisy);
         setValueToHist(hist_third, color_noisy);
 
